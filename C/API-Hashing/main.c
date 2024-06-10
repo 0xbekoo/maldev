59893d0358171e7f5d6966918e3008c23624fc89
bekoo <devtborne@gmail.com>
2024-06-10 20:58:06 +0300

    Add files via upload

---
+++ C/API-Hashing/main.c
@@ -0,0 +1,75 @@
+﻿#include "utils.h"
+
+DWORD GetHash(char* ApiString) {
+	size_t	APILen	  = strnlen_s(ApiString, 50);
+	DWORD	Hash	  = 0x75;
+	int		x		  = 0;
+
+	do {
+		Hash += (Hash * 0xab19f29f + ApiString[x]) & 0xffffff;
+		x++;
+	} while (x < APILen);
+	return Hash;
+}
+
+PDWORD GetFunctionAddress(char* ApiLibrary, DWORD hash)
+{
+	PDWORD functionAddress = (PDWORD)0;
+
+	HMODULE LibraryAddress = LoadLibraryA(ApiLibrary);
+	if (LibraryAddress == NULL) {
+		printf("Failed to Load Library! Error Code: 0x%lx\n", GetLastError());
+		return NULL;
+	}
+	PIMAGE_DOS_HEADER		DosHeader			 = (PIMAGE_DOS_HEADER)LibraryAddress;
+	PIMAGE_NT_HEADERS		ImageNTHeaders		 = (PIMAGE_NT_HEADERS)((DWORD_PTR)LibraryAddress + DosHeader->e_lfanew);
+	DWORD_PTR				ExportDirectory		 = ImageNTHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+	PIMAGE_EXPORT_DIRECTORY ImageExportDirectory = (PIMAGE_EXPORT_DIRECTORY)((DWORD_PTR)LibraryAddress + ExportDirectory);
+
+	// Export edilmiş fonksiyonlarla ilgili bilgiler için RVA'ları al
+	PDWORD addresOfFunctionsRVA		= (PDWORD)((DWORD_PTR)LibraryAddress + ImageExportDirectory->AddressOfFunctions);
+	PDWORD addressOfNamesRVA		= (PDWORD)((DWORD_PTR)LibraryAddress + ImageExportDirectory->AddressOfNames);
+	PWORD  addressOfNameOrdinalsRVA = (PWORD)((DWORD_PTR)LibraryAddress + ImageExportDirectory->AddressOfNameOrdinals);
+
+	for (DWORD i = 0; i < ImageExportDirectory->NumberOfFunctions; i++)
+	{
+		DWORD	  functionNameRVA	 = addressOfNamesRVA[i];
+		DWORD_PTR functionNameVA	 = (DWORD_PTR)LibraryAddress + functionNameRVA;
+		char*	  functionName		 = (char*)functionNameVA;
+		DWORD_PTR functionAddressRVA = 0;
+
+		DWORD functionNameHash = GetHash(functionName);
+		if (functionNameHash == hash)
+		{
+			functionAddressRVA = addresOfFunctionsRVA[addressOfNameOrdinalsRVA[i]];
+			functionAddress = (PDWORD)((DWORD_PTR)LibraryAddress + functionAddressRVA);
+			printf("%s (0x00%x): %p\n", functionName, functionNameHash, functionAddress);
+			return functionAddress;
+		}
+	}
+	return NULL;
+}
+ 
+int main(int argc, char* argv[]) {
+	DWORD  ApiHash		   = 0;
+	DWORD  TID			   = 0;
+	PDWORD FunctionAddress = NULL;
+
+	ApiHash = GetHash("MessageBoxA");
+	printf("0x00%x\n", ApiHash);
+
+	FunctionAddress = GetFunctionAddress((char*)"user32", ApiHash);
+	if (FunctionAddress == NULL) {
+		printf("Failed to get Api Address!\n");
+		return -1;
+	}
+
+	MyMessageBoxA MessageBoxA = (MyMessageBoxA)FunctionAddress;
+	int result = MessageBoxA(NULL, "YEPUUUU THIS IS API HASHING!", "Message", MB_OK);
+	if (result == 0) {
+		printf("Failed to Show Message Box! Error Code: 0x%lx\n", GetLastError());
+		return -1;
+	}
+	printf("Message Box Shown!\n");
+	return 0;
+}
---
+++ C/API-Hashing/utils.h
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+typedef int (WINAPI* MyMessageBoxA)(
+    HWND    hWnd,
+    LPCSTR  lpText,
+    LPCSTR  lpCaption,
+    UINT    uType
+ );
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _LDR_MODULE {
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID BaseAddress;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+    ULONG Flags;
+    USHORT LoadCount;
+    USHORT TlsIndex;
+    LIST_ENTRY HashTableEntry;
+    ULONG TimeDateStamp;
+} LDR_MODULE, * PLDR_MODULE;
+
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+    LIST_ENTRY InLoadOrderLinks;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID DllBase;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+    ULONG Flags;
+    USHORT LoadCount;
+    USHORT TlsIndex;
+    union
+    {
+        LIST_ENTRY HashLinks;
+        struct
+        {
+            PVOID SectionPointer;
+            ULONG CheckSum;
+        };
+    };
+    union
+    {
+        ULONG TimeDateStamp;
+        PVOID LoadedImports;
+    };
+    PVOID EntryPointActivationContext;
+    PVOID PatchInformation;
+} LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
